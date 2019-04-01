991589e45713736b497ce6502f07e230125d9fd4
afriestad <afriestad@users.noreply.github.com>
2019-04-01 09:09:57 +0200

    Make initial migration for course_admin

--- qu/course_admin/models.py
+++ qu/course_admin/models.py
@@ -8,10 +8,10 @@ class Course(models.Model):
     name = models.CharField(max_length=127, null=False)
     course_code = models.CharField(max_length=16, null=False)
     semester = models.CharField(max_length=3, null=False)
-    admins = models.ForeignKey(User, on_delete=models.SET_NULL)
-    teaching_assistants = models.ForeignKey(User, on_delete=models.SET_NULL)
-    students = models.ForeignKey(User, on_delete=models.SET_NULL)
-    year = models.DateField(default=now())
+    admins = models.ForeignKey(User, related_name='taught_courses', on_delete=models.DO_NOTHING)
+    teaching_assistants = models.ForeignKey(User, related_name='assisted_courses', on_delete=models.DO_NOTHING)
+    students = models.ForeignKey(User, related_name='taken_courses', on_delete=models.DO_NOTHING)
+    year = models.DateField(default=now)
 
 
 class Room(models.Model):
