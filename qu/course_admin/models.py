904656eeefa1762983404702e11ee4bb53adbfc2
afriestad <afriestad@users.noreply.github.com>
2019-04-04 14:14:08 +0200

    Remove comment from course_admin/models

--- qu/course_admin/models.py
+++ qu/course_admin/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.utils.timezone import now
 
 
-# Create your models here.
 class Course(models.Model):
     name = models.CharField(max_length=127, null=False)
     course_code = models.CharField(max_length=16, null=False)
