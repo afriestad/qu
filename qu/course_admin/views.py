323b36ec66457e955a3db6f3e850ea02d66f8baa
afriestad <afriestad@users.noreply.github.com>
2019-04-05 17:36:50 +0200

    Protect IndexView behind login

--- qu/course_admin/views.py
+++ qu/course_admin/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
-class IndexView(View):
+class IndexView(LoginRequiredMixin, View):
     template_name = 'course_admin/index.html'
 
     def get(self, request, *args, **kwargs):
